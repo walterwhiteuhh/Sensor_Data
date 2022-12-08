582eeeac24b4bbb727e6dea5863ae980f61e3524
Timo <timo.fischer@protonmail.com>
2022-12-08 01:08:48 +0100

    Add get data function to main.py

--- appDHT.py
+++ appDHT.py
@@ -10,3 +10,17 @@ dbname='sensorsData.db'
 # time in seconds
 
 sampleFreq = 2 
+
+
+# get data from DHT sensor
+
+def getDHTdata():	
+	
+	DHT22Sensor = Adafruit_DHT.DHT22
+	DHTpin = 16
+	hum, temp = Adafruit_DHT.read_retry(DHT22Sensor, DHTpin)
+	
+	if hum is not None and temp is not None:
+		hum = round(hum)
+		temp = round(temp, 1)
+		logData (temp, hum)
